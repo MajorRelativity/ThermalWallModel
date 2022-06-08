e442be45a11f294402583d3dd7ae2539ba727f4f
MajorRelativity <80081144+MajorRelativity@users.noreply.github.com>
2022-06-08 15:42:24 -0400

    V R0.04 - Fail (TCw,TMw,TSw)
    When I added in what I thoght to be the accurate measurements for the thermal conductivity, mass density, and specific heat, the model became meaningless even when I made the time as long as possible

--- ThermalWallModelR.m
+++ ThermalWallModelR.m
@@ -14,9 +14,14 @@ axis equal
 
 %% Thermal Properties:
 
-thermalProperties(thermalmodel,'ThermalConductivity',1,...
-                               'MassDensity',1,...
-                               'SpecificHeat',1);
+%Wall
+TCw = .03; % Thermal Conductivity for the Wall W/(m*K)
+TMw = .96 * ((100^3)/(10^3)); % Mass Density for the Wall kg/m^3
+TSw = 1100 * (10^3); % Specific Heat for the Wall J / kg * K
+
+thermalProperties(thermalmodel,'ThermalConductivity',TCw,...
+                               'MassDensity',TMw,...
+                               'SpecificHeat',TSw);
 
 HF = (1.82)*1055/((60*60)*(.305^2))
 
@@ -35,7 +40,7 @@ title('Mesh with Quadratic Triangular Elements')
 
 %% Set Times and Solve the Model:
 
-tlist = 0:0.5:5;
+tlist = 0:50:10^7;
 thermalresults = solve(thermalmodel,tlist)
 [qx,qy] = evaluateHeatFlux(thermalresults);
 
