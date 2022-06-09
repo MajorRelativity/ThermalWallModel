33ee66ae66748e0117bc213444db38de1f85cabc
MajorRelativity <80081144+MajorRelativity@users.noreply.github.com>
2022-06-09 16:08:26 -0400

    V R0.11 - Preferences
    Cleaned up user interface and added preferences for certain features.

--- ThermalWallModelR.m
+++ ThermalWallModelR.m
@@ -1,7 +1,17 @@
-%% ThermalWallModel Version R0.10
+%% ThermalWallModel Version R0.11
 % Updated on June 9 2022
 % Code take from MatLab demonstration on how to model a wall with a crack
 % in it.
+
+%% Initialization and Preferences:
+
+clear
+
+%Preferences:
+qSM = 1; %Show Mesh and Geometry (1 = yes, 0 = no)
+qSMpm = 3; %Mesh Pause Duration
+qSMpw = 1; %Wall Pause Duration
+qPss = 1; %Plot Steady State Animation
 
 %% User Edited Section:
 
@@ -27,13 +37,14 @@ TempwO = 297; %Outdoor Wall Temperature K
 Tempi = 300; %Interior Temperature K
 
 %Time Conditions:
-timeE = 100000; %End Time s
-timeStep = 100000; %The step between when the model calculates s
+timeE = 1000; %End Time s
+timeStep = 5; %The step between when the model calculates s
 
 %Plot Conditions:
-timeStepP = 100000; %The step in between plots s
+timeStepP = 60; %The step in between plots s
 PauseP = .1; %The time in between frames during plot s
 qPF = 1; % 1 = display in seconds, 2 = display in minutes, 3 = display in hours
+
 %% Initialization
 close
 tf = FoamThickness;
@@ -46,10 +57,17 @@ wallGeometry(Lw,Tw,lf,tf)
 thermalmodel = createpde('thermal','transient');
 
 %% Importing and Ploting Geometry
+disp('[$] Importing Geomerty and Applying Conditions')
 geometryFromEdges(thermalmodel,@modelshapew);
-figure(1)
-axis equal
+Fwg = figure('Name','Wall Geometry');
+Fwg.Visible = "off";
 pdegplot(thermalmodel,'EdgeLabels','on')
+xaxis = [0,Tw+tf+Tw];
+yaxis = [-3*Lw/4,3*Lw/4];
+axis([xaxis,yaxis])
+hold on
+axis square
+hold off
 
 %% Thermal Properties:
 
@@ -70,88 +88,108 @@ thermalBC(thermalmodel,'Edge',[3,5,7],'Temperature',TempwO);
 
 %thermalIC(thermalmodel, TempwO,'Edge',[3,5,7])
 thermalIC(thermalmodel,Tempi);
-%% Generate Mesh
 
+disp('[+] Geomerty Imported and Conditions Applied')
+%% Generate Mesh
+disp('[$] Generating Mesh')
 Mesh = generateMesh(thermalmodel,'Hmin',.001,'Hmax',.01);
-figure(2)
+Fmg = figure('Name','Mesh Geomerty');
+Fmg.Visible = "off";
 pdemesh(thermalmodel)
 title('Mesh with Quadratic Triangular Elements')
-
+disp('[+] Mesh Generated')
 
 %% Set Times and Solve the Model:
 
 tlist = 0:timeStep:timeE; %24 hours
-thermalresults = solve(thermalmodel,tlist)
+thermalresults = solve(thermalmodel,tlist);
 [qx,qy] = evaluateHeatFlux(thermalresults);
+disp('[+] Thermal Model Solved')
 
 %% Plot Temperature and Heat Flux:
 
-pause(1)
-close(2)
-pause(1)
-close(1)
-
-%Initial Setup:
-M = timeStepP/timeStep; %Time Skip
-P = PauseP;
-cM = 1;
-
-F = figure;
-F.Visible = "off";
-n = M*(0:((size(tlist,2)-1)/M))+1;
-Fssp(size(n,2)) = struct('cdata',[],'colormap',[]);
-
-%Plot Animation
-for n = n
-    
-    %Figure Name (seconds, minutes, or hours)
-    if qPF == 2
-        Fname = [num2str(tlist(n)/60),' minutes in'];
-    elseif qPF == 3
-        Fname = [num2str(tlist(n)/(60*60)),' hours in'];
-    else
-        Fname = [num2str(tlist(n)),' seconds in'];
-    end
-
-    %Plot Figure:
-    F.Name = Fname;
-    pdeplot(thermalmodel,'XYData',thermalresults.Temperature(:,n), ...
-                     'Contour','on',...
-                     'FlowData',[qx(:,n),qy(:,n)], ...
-                     'ColorMap','hot')
-    title(Fname)
-
-    %Store Figure:
-    ax = gca; %axis
-    ax.Units = 'pixels';
-    axpos = ax.Position;
-    marg1 = -30;
-    marg2 = -20;
-    marg3 = 100;
-    marg4 = 100;
-    %rect = [axpos(1)+marg1, axpos(2)+marg2, axpos(3)+marg3, axpos(4)+marg4];
-    Fssp(cM) = getframe(F); %Fssp = Stores Progression to Steady State
-    ax.Units = 'normalized';
-
-
-
-    %Repeat:
-    cM = cM + 1;
+% Show Mesh and Geometry
+if qSM == 1
+    Fwg.Visible = "on";
+    Fmg.Visible = "on";
+    disp(['[@] Displaying Mesh Geometry for ',num2str(qSMpm),' second(s):'])
+    pause(qSMpm)
+    close(Fmg)
+    disp(['[@] Displaying Wall Geometry for ',num2str(qSMpw),' second(s):'])
+    pause(qSMpw)
+    close(Fwg)
+else
+    close(Fmg)
+    close(Fwg)
 end
 
-% Return figure visibility and play movie:
-F.Visible = "on";
-figure(2)
-movie(gcf,Fssp)
+%Initial Setup:
+if qPss == 1
+    M = timeStepP/timeStep; %Time Skip
+    P = PauseP;
+    cM = 1;
+    
+    F = figure;
+    F.Visible = "off";
+    n = M*(0:((size(tlist,2)-1)/M))+1;
+    Fssp(size(n,2)) = struct('cdata',[],'colormap',[]);
+    
+    %Plot Animation
+    for n = n
+        
+        %Figure Name (seconds, minutes, or hours)
+        if qPF == 2
+            Fname = [num2str(tlist(n)/60),' minutes in'];
+        elseif qPF == 3
+            Fname = [num2str(tlist(n)/(60*60)),' hours in'];
+        else
+            Fname = [num2str(tlist(n)),' seconds in'];
+        end
+    
+        %Plot Figure:
+        F.Name = Fname;
+        pdeplot(thermalmodel,'XYData',thermalresults.Temperature(:,n), ...
+                         'Contour','on',...
+                         'FlowData',[qx(:,n),qy(:,n)], ...
+                         'ColorMap','hot')
+        xaxis = [0,Tw+tf+Tw];
+        yaxis = [-3*Lw/4,3*Lw/4];
+        axis([xaxis,yaxis])
+        title(Fname)
+        xlabel('Meters')
+        ylabel('Meters')
+    
+        %Store Figure:
+        Fssp(cM) = getframe(F); %Fssp = Stores Progression to Steady State
+    
+        % Inform User and Repeat:
+        cM = cM + 1;
+        FsspProgress = num2str((tlist(n)/tlist(end))*100); %Creates a Progress Bar
+        disp(['[*] Building Steady State Creation Animation: ',FsspProgress,'% Complete'])
+    end
+    
+    disp('[+] Steady State Creation Animation Completed')
+    
+    % Return figure visibility and play movie:
+    close(F)
+    figure('Name','Steady State Creation Animation')
+    disp('[@] Playing "Steady State Creation Animation" (Fssp)')
+    movie(gcf,Fssp,2,5)
+end
 
-%% Find Temperature at Point Between Foam:
-
+%% Find Temperature at Point Between Foam Using interpolateTemperature:
+disp('[$] Finding Temperatures in Between the Foam and Wall')
 Y = linspace(-lf/2,lf/2,11);
 cI = 1;
 for n = Y
-TempIntersect(cI) = interpolateTemperature(thermalresults,Tw,n,...
-    2) %interpolates temperature at (Tw,Y)
-cI = cI+1
+TempAtIntersect(cI) = interpolateTemperature(thermalresults,Tw,n,...
+    2); %interpolates temperature at (Tw,Y)
+cI = cI+1;
 end
 
+FI = [Y(2:end-1)',TempAtIntersect(2:end-1)']; %Returns array with first column being y values and second column being temperature values
+FITable = array2table(FI);
+FITable.Properties.VariableNames(1:2) = {'Y-Value','Temperature'};
+
+disp('[+] Temperatures Have Been Found and Stored in Array "FI" and Table "FITable"')
 
