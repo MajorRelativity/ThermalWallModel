d491388a204fbc30e2de627e0b8385ff93f7a5b1
MajorRelativity <80081144+MajorRelativity@users.noreply.github.com>
2022-07-08 17:05:21 -0400

    v 2.A33 - preP Change
    - No longer push DS-Store
    - prePIndex creation has now been outsourced to another function
    - Started a list of debug commands

--- ThermalWallModel2A33/ThermalWallModel.m
+++ ThermalWallModel2A33/ThermalWallModel.m
@@ -1,5 +1,5 @@
-%% ThermalWallModel v2.A32
-% Updated on July 7 2022
+%% ThermalWallModel v2.A33
+% Updated on July 8 2022
 
 clear
 addpath("Functions")
@@ -9,8 +9,13 @@ addpath("Functions")
 %{
 
 Process ID:
+-00) Debug:
+
+    -01) Quit
+    -02) Get Collection Programs
+
 000) Collection
-  
+
     001 - 050 = 3D Model:
         Standard:
             001) Generate Single Geometry
@@ -255,6 +260,7 @@ ColstrT4 = '\n    Solve Models';
 
 ColstrInput = '\n  Input: ';
 ColstrQuit = '\n -1 = Quit ';
+ColstrDebug2 = '\n -2 = Debug: Show Collection Programs ';
 ColstrRun = '\n  0 = Run with Nothing Else ';
 Colstr3DT = '\n  1 - 50: 3D Model';
 
@@ -296,10 +302,13 @@ while gateC == 1
         case 1
             qCollection(numC) = input(['[?] What would you like to do?',Colstr,ColstrQuit,ColstrInput]);
         otherwise
-            qCollection(numC) = input(['[?] What else would you like to do?',Colstr,ColstrQuit,ColstrRun,ColstrInput]);
+            qCollection(numC) = input(['[?] What else would you like to do?',Colstr,ColstrDebug2,ColstrQuit,ColstrRun,ColstrInput]);
     end
     
     switch qCollection(numC)
+        case -2
+            disp('[&] Initiating Debug Mode: Getting programs for all requested collections.')
+            gateC = 0;
         case -1
             disp('[~] Quitting Script')
 
@@ -318,254 +327,8 @@ end
 % Clear Collection Names:
 clear -regexp Colstr
 
-% Preallocate Varaibles: 
-
-maxpreP = 10;
-numC = 1;
-
-% Create Pre-Run Process Index:
-
-for C = qCollection
-    
-    switch C
-        case 0
-            % Ignore
-            numC = numC - 1;
-            break
-        case 1            
-            % Program #1 - Generate Geometry
-            prePline = [101 119 103 104 107 112 114 1]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-       case 2          
-            % Program #2 - Run Model From Geometry
-            prePline = [101 119 104 105 117 106 109 118 107 2]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-       case 3          
-            % Program #3 - Contour Slices
-            prePline = [101 119 107 3]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-        case 4
-            % Program #4 - Get Temperature at Point
-            prePline = [101 119 107 4]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-       case 51            
-            % Program #51 - 2D Generate Geometry
-            prePline = [101 119 112 111 104 108 114 51]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-       case 52          
-            % Program #52 - 2D Run Model From Geometry
-            prePline = [101 119 104 105 116 106 109 118 108 52]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-       case 53          
-            % Program #53 - 2D Contour Plot
-            prePline = [101 119 108 53]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-        case 54
-            % Program #54 - 2D Get Temperature at Point
-            prePline = [101 119 108 54]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-        case 55
-            % Program #55 - 2D Generate Single Geometry with Stud
-            prePline = [101 119 111 104 108 112 113 55]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-        case 56
-            % Program #56 - 2D Plot Current Thermal Properties
-            prePline = [101 119 108 112 113 56]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-        case 57      
-            % Program #57 - 2D Generate All Geometries with Stud
-            prePline = [101 119 111 104 108 112 115 57]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-        case 58          
-            % Program #58 - 2D Solve All Stud Analysis Models
-            prePline = [101 119 104 105 116 106 109 118 108 58]; %prePrograms always end with their program ID #
-
-            % Add zeros if program size is less than max size
-
-            if size(prePline,2) < maxpreP
-                prePline = [prePline, zeros(1,maxpreP - size(prePline,2))];
-            elseif size(prePline,2) > maxpreP
-                error(['[!] Max preProgram Size MUST be updated to ',num2str(size(prePline,2))])
-            end
-
-            % Concatonate to P
-
-            if exist('preP','var')
-                preP = [preP;prePline];
-            else
-                preP = prePline;
-            end
-
-    end
-    
-    numC = numC + 1;
-    
-end
+%% Create Pre-Run Process Index:
+[preP,numC] = preRunIndex(qCollection);
 
 %% Prerun:
 
@@ -578,6 +341,13 @@ maxP = 18;
 for preI = 1:size(preP,1)
     for prep = preP(preI,:)
         switch prep
+            case -2
+                disp('[+] [-02] Displaying preP:')
+                disp(preP)
+                disp('[+] [-02] Displaying P:')
+                disp(P)
+                disp('[~] [-02] Quitting Script:')
+                return
             case 0
                 % Ignore
                 break
@@ -1383,7 +1153,7 @@ for I = 1:size(P,1)
                     Logs.numMi = numM;
                 end
                 
-                disp(['[+] [505] ','numMi determined to be:',num2str(Logs.numMi)])
+                disp(['[+] [505] ','numMi determined to be: ',num2str(Logs.numMi)])
 
             case 506
                 % Add to Foam Analysis Result Log
